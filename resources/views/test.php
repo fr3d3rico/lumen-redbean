50b7d62a2a185a4f7d9e15a79c755d026451181f
Frederico Araujo Soares <fasoares@tjgo.gov>
2019-07-08 16:44:07 -0300

    Update for initial tests.

---
+++ resources/views/test.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        Hello, <?php echo $name; ?>
+        <br />
+        <a href='http://localhost:8080/view'>/view</a>
+        <br />
+        <a href='http://localhost:8080'>/</a>
+        <br />
+        <a href='http://localhost:8080/fred'>/fred</a>
+        <br />
+        <a href='http://localhost:8080/save'>/save</a>
+        <br />
+        <a href='http://localhost:8080/load/xxxxxxx'>/load{id}</a>
+        <br />
+        <a href='http://localhost:8080/load'>/load</a>
+        <br />
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use \RedBeanPHP\R;
+
+R::setup('mysql:host=localhost;port=3306;dbname=homestead','root','');
+
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -13,4 +18,48 @@
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->get('/view', function () use ($router) {
+    return view('test', ['name' => 'Fred']);
+});
+
+$router->get('/fred', function () use ($router) {
+    return "fred";
+});
+
+$router->get('/save', function () use ($router) {
+    
+    $tool = R::dispense("tools");
+
+    $tool->name = "Fred";
+    $tool->link = "link";
+
+    $id = R::store($tool);
+
+    return "save: " . $id;
+});
+
+$router->get('/load', function () {
+    $tools = R::findAll('tools');
+    $table = "<table border='1'><thead><tr><th>#</th><th>data</th></tr></thead><tbody>";
+    if( $tools ) {
+        foreach($tools as $index => $item) {
+            $table .= "<tr><td>".$index."</td><td>id".($item->id) . $item ."</td></tr>";
+        }
+        $table .= "</tbody></table>";
+    }
+    return $table;
+});
+
+$router->get('load/{id}', function ($id) {
+    $tool = R::load("tools", $id);
+    return $tool;
+});
+
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->get('/auth/user', function () {
+        // Uses Auth Middleware
+        return "auth";
+    });
 });
