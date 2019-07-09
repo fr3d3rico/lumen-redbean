51ddb82f8b447360db3adb0f246154a3d5cfb371
Fred <frederico.a.soares@gmail.com>
2019-07-08 22:30:20 -0300

    Update migration file

---
+++ database/migrations/2019_07_09_002914_create-tools-table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tools', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('link');
+            $table->string('description');
+            $table->string('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tools');
+    }
+}
